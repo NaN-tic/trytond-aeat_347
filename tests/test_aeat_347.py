e691718e0a6a1fbea448bb808ff3e1c49c1d28d6
Raimon Esteve <raimonesteve@gmail.com>
2022-03-22 17:56:05 +0100

    Check rule only if _check_access is set and enforce companies rule issue4080

--- tests/test_aeat_347.py
+++ tests/test_aeat_347.py
@@ -7,9 +7,10 @@ import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_teardown
 from trytond.tests.test_tryton import doctest_checker
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class Aeat347TestCase(ModuleTestCase):
+class Aeat347TestCase(CompanyTestMixin, ModuleTestCase):
     'Test Aeat 347 module'
     module = 'aeat_347'
 
