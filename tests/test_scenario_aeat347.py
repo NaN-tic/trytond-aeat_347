14cd23340206f198e557aca9e1d432073564eb53
Bernat Brunet <bernat@nan-tic.com>
2025-11-23 09:47:30 +0100

    Fix test. Us the default functions of the account_es not the account.

--- tests/test_scenario_aeat347.py
+++ tests/test_scenario_aeat347.py
@@ -3,9 +3,9 @@ import unittest
 from decimal import Decimal
 
 from proteus import Model, Wizard
-from trytond.modules.account.tests.tools import (create_chart,
-                                                 create_fiscalyear, create_tax,
-                                                 get_accounts)
+from trytond.modules.account_es.tests.tools import (create_chart, create_tax,
+    get_accounts)
+from trytond.modules.account.tests.tools import create_fiscalyear
 from trytond.modules.account_invoice.tests.tools import (
     create_payment_term, set_fiscalyear_invoice_sequences)
 from trytond.modules.company.tests.tools import create_company, get_company
